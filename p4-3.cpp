53046f9301a974ec0061a9099e65757e230d41b9
poojalingayatt <poojalingayatt@gmail.com>
2025-05-07 15:41:21 +0530

    Update p4-3.cpp

--- p4-3.cpp
+++ p4-3.cpp
@@ -1,51 +1,65 @@
+/* Programmer: Pooja Lingayat
+   Date: 3-3-25
+   Aim: To implement multiple inheritance by managing car details using separate base classes for fuel type and brand name. */
+
 #include<iostream>
 #include<string>
 using namespace std;
 
 // Base class for fuel type
-class Fuel {
+class Fuel
+{
 protected:
     string fuelType;
 public:
-    Fuel(string fuel = "") {
+    Fuel(string fuel = "")
+    {
         fuelType = fuel;
     }
-    void displayFuel() {
+    void displayFuel() 
+    {
         cout << "Fuel Type: " << fuelType << endl;
     }
 };
 
 // Base class for brand name
-class Brand {
+class Brand
+{
 protected:
     string brandName;
 public:
-    Brand(string brand = "") {
+    Brand(string brand = "") 
+{
         brandName = brand;
     }
-    void displayBrand() {
+    void displayBrand() 
+    {
         cout << "Brand Name: " << brandName << endl;
     }
 };
 
 // Derived class combining Fuel and Brand
-class Car : public Fuel, public Brand {
+class Car : public Fuel, public Brand 
+{
 public:
     Car(string fuel = "", string brand = "") : Fuel(fuel), Brand(brand) {}
-    void displayCar() {
+    void displayCar()
+   {
         displayBrand();
         displayFuel();
     }
 };
 
-int main() {
+int main()
+  {
     int n;
     cout << "Enter number of cars in service queue: ";
     cin >> n;
 
     Car cars[100];  // fixed size array, assuming max 100 cars
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         string fuel, brand;
 
         cout << "\nEnter details for Car " << i + 1 << ":" << endl;
@@ -60,7 +74,8 @@ int main() {
     }
 
     cout << "\n--- Service Queue Details ---\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << "\nCar " << i + 1 << " Details:" << endl;
         cars[i].displayCar();
     }
